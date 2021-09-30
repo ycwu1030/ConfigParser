ec9a377cba1ba97c56caf44c186eaa5077a4b8f1
Yongcheng Wu <yc_wu1030@126.com>
2021-09-30 15:17:02 -0500

    func(Convert): Convert Class with static member function
    Convert Class with static member function to convert between string and specific type
    For string type, do it with caution

---
+++ include/Conversion.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+class Convert {
+public:
+    template <typename T>
+    static std::string T_to_string(T const &val) {
+        std::ostringstream ostr;
+        ostr << val;
+        return ostr.str();
+    }
+
+    template <typename T>
+    static T string_to_T(std::string const &val) {
+        std::istringstream istr(val);
+        T returnVal;
+        if (!(istr >> returnVal)) {
+            exitWithError("ConfigParser: Not a valid " + static_cast<std::string> typeid(T).name() + " received!\n");
+        }
+        return returnVal;
+    }
+
+    static std::string string_to_T(std::string const &val) { return val; }
+};
